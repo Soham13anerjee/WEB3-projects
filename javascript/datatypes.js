593b343d51159e68c755be10e26d80fe1bade5ea
Soham13anerjee <sohambanerg@gmail.com>
2023-09-03 13:12:14 +0530

    Javascript commit 1

---
+++ javascript/datatypes.js
@@ -0,0 +1,31 @@
+// datatypes 2 types - primitive and non primitive (classes n objects)
+// strings can have double quotes, single quotes and even back ticks `
+
+const name = 'soham'
+const college = 'iiit'
+const stream = 'ece'
+
+ // 3 datatypes are - strings shown above, numbers that represent integers and floating point numbers and booleans which are only either of true or false
+
+ //numbers
+ const total=0
+ const PI = 3.14
+
+ //booleans
+ const isPrim = true
+
+
+ // undefined where value is not assigned at declaration
+
+ let vari
+ //undefined
+ console.log(vari)
+
+ const a = undefined
+
+ // null datatype  to assign a value not known, so a better option to prev code in line 24 is
+
+ const b = null
+
+ // other primitive types are BigInt and Symbol datatypes
+ 
---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+// learn use of console log function
+console.log('Hello from main.js')
+
+// Variables
+let age = 25
+console.log(age)
+
+// let lets you change the variable later
+
+let sum 
+sum = 0
+
+console.log(sum)
+
+// const keyword for declaring constant variables.. 
+// remember, const cant be initialised like const x, you have to assign a value initially.
+
+const salary = 80000
+console.log(salary)
