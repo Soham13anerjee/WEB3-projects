f7b5fb6f2ddba86914914275039313711c625f2d
Soham13anerjee <sohambanerg@gmail.com>
2023-09-03 18:55:56 +0530

    datatypes.js additions

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -29,3 +29,33 @@ const stream = 'ece'
 
  // other primitive types are BigInt and Symbol datatypes
  
+ // arrays -> any type of datatypes can be included in arrays
+
+let ar = [9,true,"explicit"]
+
+console.log(ar)
+
+console.log(ar[0]) //access array elements
+
+// undefined used to define anything not initialized
+let x
+console.log("before initializing")
+console.log(x)
+x=22
+console.log("after initializing")
+console.log(x)
+
+const v = undefined // this too is possible
+
+const bigIntNum = 1129n // write small n after no literal, and the moment user does that, datatype changes from ineteger to big integer
+
+// normal int datatype -> 2^53 size which is a huge number of magitude i.e. size n range
+
+// another such variable is null to explicitly mention this variable or value should'nt or does'nt exist. but undefined means just that this variable has'nt been initialized yet, but can be initialized afterwards anytime
+
+// typeof gives us string data
+const useOfTypeof = typeof ar // object. cause array in JS is object type
+
+typeof null // object
+typeof undefined // undefined
+console.log(typeof 1111n) //bigint
